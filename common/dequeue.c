13cc4e34bfb3a3d2de5bf7e48f1d23c2ae659630
dimas-ady <dimasady123@outlook.co.id>
2026-02-21 22:28:15 +0700

    add dequeue type data

---
+++ common/dequeue.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "dequeue.h"
+
+int dequeue_is_empty(Dequeue *dequeue) {
+    return (dequeue->head == NULL && dequeue->tail == NULL);
+}
+
+void deqeueue_push_front(Dequeue *dequeue, int data) {
+    DequeueNode *new_node = malloc(sizeof(DequeueNode));
+    new_node->data;
+
+    if(dequeue_is_empty(dequeue)) {
+        dequeue->size = 0;
+        dequeue->head = new_node;
+        dequeue->tail = new_node;
+    } else {
+        new_node->next = dequeue->head;
+        dequeue->head->prev = new_node;
+        dequeue->head = new_node;
+    }
+    dequeue->size++;
+}
+
+void deqeueue_push_back(Dequeue *dequeue, int data) {
+    DequeueNode *new_node = malloc(sizeof(DequeueNode));
+    new_node->data;
+
+    if(dequeue_is_empty(dequeue)) {
+        dequeue->size = 0;
+        dequeue->head = new_node;
+        dequeue->tail = new_node;
+    } else {
+        new_node->prev = dequeue->tail;
+        dequeue->tail->next = new_node;
+        dequeue->tail = new_node;
+    }
+    dequeue->size++;
+}
+
+int *dequeue_front(Dequeue *dequeue) {
+    if(!dequeue_is_empty(dequeue))
+        return dequeue->head->data;
+    else
+        return NULL;
+}
+
+int *dequeue_back(Dequeue *dequeue) {
+    if(!dequeue_is_empty(dequeue))
+        return dequeue->tail->data;
+    else
+        return NULL;
+
+}
+
+void deqeueue_pop_front(Dequeue *dequeue) {
+    DequeueNode *front = dequeue->head;
+    dequeue->head = front->next;
+    if(dequeue->head != NULL)
+        dequeue->head->prev = NULL;
+
+    free(front);
+    dequeue->size--;
+
+    if(dequeue->head == NULL) {
+        dequeue->tail = NULL;
+        dequeue->size = 0;
+    }
+}
+
+void dequeue_pop_back(Dequeue *dequeue) {
+    DequeueNode *back = dequeue->tail;
+    dequeue->tail = back->prev;
+    if(dequeue->tail != NULL)
+        dequeue->tail->next = NULL;
+
+    free(back);
+    dequeue->size--;
+
+    if(dequeue->tail == NULL) {
+        dequeue->head = NULL;
+        dequeue->size = 0;
+    }
+
+}
---
+++ common/dequeue.h
@@ -0,0 +1,30 @@
+#ifndef DEQUEUE_H
+#define DEQUEUE_H
+
+typedef struct DequeueNode DequeueNode;
+
+struct DequeueNode{
+    int data;
+    DequeueNode *next, *prev;
+};
+
+typedef struct {
+    unsigned size;
+    DequeueNode *head, *tail;
+} Dequeue;
+
+int dequeue_is_empty(Dequeue *dequeue);
+
+void deqeueue_push_front(Dequeue *dequeue, int data);
+
+void deqeueue_push_back(Dequeue *dequeue, int data);
+
+int *dequeue_front(Dequeue *dequeue);
+
+int *dequeue_back(Dequeue *dequeue);
+
+void deqeueue_pop_front(Dequeue *dequeue);
+
+void dequeue_pop_back(Dequeue *dequeue);
+
+#endif
